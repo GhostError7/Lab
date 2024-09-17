563b886a4650621f8871d6dec8ae5cef8f3a507e
GhostError7 <mikjanets.alex@gmail.com>
2024-09-17 18:48:40 +0300

    Add files via upload

---
+++ Lab2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string birth;
+            const int Year = 2024;
+            Console.WriteLine("Введите свой год рождения");
+            birth = Console.ReadLine();
+            Console.BackgroundColor = ConsoleColor.Cyan;
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine("Вы родились в " + birth + " году. Сейчас " + Year + " год.");
+            Console.ReadLine();
+        }
+    }
+}
